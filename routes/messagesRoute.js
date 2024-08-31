44828e85bb237993f0ccc7fd138084be56cab492
rushikesh24 <rushikesh@8848digital.com>
2024-08-31 20:29:01 +0530

    fix: remove unused code

--- routes/messagesRoute.js
+++ routes/messagesRoute.js
@@ -6,6 +6,6 @@ const router = express.Router();
 
 router.get("/:id", isAuthenticated, getMessages);
 router.post("/send/:id", isAuthenticated, sendMessage);
-router.post("/send/:id", isAuthenticated, sendMessage);
+// router.post("/send/:id", isAuthenticated, sendMessage);
 
 export default router;
